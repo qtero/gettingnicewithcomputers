5868d3c247b53d1c0bd3aada055663935783a4d1
Krhljs <edgar.quintt@gmail.com>
2021-04-02 12:42:05 -0400

    1st attempt

--- test.py
+++ test.py
@@ -1,9 +1,17 @@
-# print("Hello World") - First 
+# print("Hello World") - First ever python code written
+
+# Assignment 1 Coingecko: Run the program to get the highest value of NDX over the last 5 days
+
 # api.coingecko.com/api/v3 - Host URL
 import requests
 import json
-payload = {'date':'11-03-2021'}
+payload = {'date':'28-03-2021'}
 r = requests.get('https://api.coingecko.com/api/v3/coins/indexed-finance/history', params=payload)
 json_object = json.loads(r.text)
-json_formatted_str = json.dumps(json_object, indent=2)
+indexed_USD_Value = float(json.dumps(json_object['market_data']['current_price']['usd'], indent=2)) #Uses "keys" to pull only specifc data within the dict, this this data set we are only looking for the current USD price according to date and then converts that current str into a value
+
+#build statements to rerun code above looking for the highest value in past 5 days
+if indexed_USD_Value >
+
+
 print(json_formatted_str)
